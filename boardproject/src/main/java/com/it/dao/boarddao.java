165e2a139f1cb4e4762a3b0e69f59786330b1449
leejaehyun <kkao45617@naver.com>
2022-06-07 14:17:54 +0900

    2022-06-07

--- boardproject/src/main/java/com/it/dao/boarddao.java
+++ boardproject/src/main/java/com/it/dao/boarddao.java
@@ -1,111 +1,177 @@
 package com.it.dao;
 
-import java.util.*; //list
-import java.sql.*; //connection, prepdaestatment, resultset
+import java.util.*;  //List
+import java.util.Date;
+import java.sql.*;  //Connection.PrepdaedStatment, ResultSet
+
 
 public class boarddao {
-	private Connection conn=null;
-	private PreparedStatement ps = null;
-	
-	private final String url = "jdbc:mysql://localhost:3306/db0416?serverTimezone=UTC";
-	private final String username="root";
-	private final String password="1234";
-	
-	
-	//드라이버등록
-	public boarddao() {
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-		} catch (Exception e) {
-			e.printStackTrace();
+   private Connection conn=null;
+   private PreparedStatement ps = null;
+   
+   private final String URL = "jdbc:mysql://localhost:3306/db0416?serverTimezone=UTC";
+   private final String USERNAME = "root";
+   private final String PASSWORD = "1234";
+   
+   
+   //드라이브 등록
+   public boarddao() {
+      try {
+         
+         Class.forName("com.mysql.cj.jdbc.Driver"); //드라이브 메모리 활당
+      }catch(Exception e) {
+         e.printStackTrace();
+      }
+   }
+   
+   //MySql 연결
+   public void getConnection() {
+      try {
+         
+         conn = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+      }catch(Exception e) {
+         e.printStackTrace();
+      }
+   }
+   
+   //MySql해제
+   public void disConnection() {
+      try {
+         if(ps != null) ps.close();
+         if(conn != null) conn.close();
+      }catch(Exception e) {
+         e.printStackTrace();
+      }
+   }
+   
+   
+   //1. 목록출력
+    // 게시물 정보 한개 : BoardVO, 게시물 여러개 : (List)
+   public List<boardvo> boardListData(int page){
+      List<boardvo> list = new ArrayList<boardvo>();
+      
+      try {
+         
+         //1. 연결
+         getConnection();
+         //2. SQL문장 작성
+         int rowSize = 10;
+         int start = (rowSize*page) - rowSize;  
+         // 10*1 - 10 = 0,10 ..... 10*2-10=10,10, 10*3-10=20,10....
+         String sql = "select no, subject, name, DATE_FORMAT(regdate, '%Y-%m-%d'), hit "  //여기 한칸
+                  + "from jspboard order by no desc limit ?, ?";
+         
+         //3. 전송
+         ps = conn.prepareStatement(sql);
+         //4. 데이타 채우기
+         ps.setInt(1, start);
+         ps.setInt(2, rowSize);
+         
+         //5. 데이타 실행
+         ResultSet rs = ps.executeQuery();
+         
+         while(rs.next()) {
+        	 boardvo vo = new boardvo();
+            vo.setNo(rs.getInt(1));
+            vo.setSubject(rs.getString(2));
+            vo.setName(rs.getString(3));
+            vo.setDbday(rs.getString(4));
+            vo.setHit(rs.getInt(5));
+            list.add(vo);
+         }
+         //6. 닫기
+         rs.close();
+      }catch(Exception e) {
+         e.printStackTrace();
+      }finally {
+         disConnection();
+      }
+      return list;
+   }
+   
+   
+   //2. 총페이지()
+   public int boardTotalPage() {
+      int total=0;
+      
+      try {
+         
+         getConnection();
+         String sql = "select ceil(count(*)/10.0) from jspboard";
+         
+         ps = conn.prepareStatement(sql);
+         ResultSet rs = ps.executeQuery();
+         
+         if(rs.next()) {
+            total = rs.getInt(1);
+         }
+         rs.close();
+         
+      }catch(Exception e) {
+         e.printStackTrace();
+      }finally {
+         disConnection();
+      }
+      
+      return total;
+   }
+   
+   //3.insert(새글 입력)
+   public void boardinsert(boardvo vo) {
+
+	   
+	   try {
+		getConnection();
+		String sql="insert into jspboard(name,subject,content,pwd,regdate) values(?,?,?,?,?)";
+		ps=conn.prepareStatement(sql);
+		ps.setString(1, vo.getName());
+		ps.setString(2,vo.getSubject());
+		ps.setString(3, vo.getContent());
+		ps.setString(4, vo.getPwd());
+		ps.executeUpdate();
 		
-		}
+	} catch (Exception e) {
+		e.printStackTrace();
+	}finally {
+		disConnection();
 	}
-	
-	//mysql 연결
-	public void getconnection() {
-		try {
-			
-			
-			conn=DriverManager.getConnection(url,username,password);
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	//mysql 해제
-	public void disconnection() {
-		try {
-			if(ps != null)ps.close();
-			if(conn != null)conn.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	//1.디비 목록 출력
-	//게시물 정보 한개 : boardvo, 게시물 여러개 : (list)
-	public List<boardvo> boardListdata(int page){
-		List<boardvo> list = new ArrayList<boardvo>();
-		try {
-			//1.연결
-			getconnection();
-			//2.sql문장 작성
-			int rowsize =10;
-			int start = (rowsize*page)-rowsize;
-			
-			String sql="select no,subject, name, DATE format(regdate,'%y-%m-%d'),hit from jspboard order by no desc limit ?,?";
-			
-			//3.전송
-			conn.prepareStatement(sql);
-			//4.데이터채우기
-			ps.setInt(1, start);
-			ps.setInt(2, rowsize);
-			
-			//5 데이터실행
-			ResultSet rs=ps.executeQuery();
-			
-			while (rs.next()) {
-				boardvo vo = new boardvo();
-				vo.setNo(rs.getInt(1));
-				vo.setSubject(rs.getString(2));
-				vo.setName(rs.getString(3));
-				vo.setDbday(rs.getString(4));
-				vo.setHit(rs.getInt(5));
-				list.add(vo);
-			}
-			//6.닫기
-			rs.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}finally {
-			disconnection();
-		}
+   }
+   
+   //자세한 게시판 정보
+   public boardvo boardetaildate(int no) {
+	   boardvo vo = new boardvo();
+	   try {
 		
-		
-		
-		return list;
+		   getConnection();
+		   
+		   //조회수 증가
+		   String sql = "update jspboard set hit = hit+1 where no=?";
+		   ps=conn.prepareStatement(sql);
+		   ps.setInt(1, no);
+		   ps.executeUpdate();
+		   
+		   
+		   sql="select no,name,subject,content,date_format(regdate,'%Y-%m-%d'),hit"
+		   +" from jspboard where no=? ";
+		   ps=conn.prepareStatement(sql);
+		   ps.setInt(1, no);
+		   ResultSet rs= ps.executeQuery();
+		   if(rs.next()) {
+			   vo.setNo(rs.getInt(1));
+			   vo.setName(rs.getString(2));
+			   vo.setSubject(rs.getString(3));
+			   vo.setContent(rs.getString(4));
+			   vo.setDbday(rs.getString(5));
+			   vo.setHit(rs.getInt(6));
+			   rs.close();
+		   }
+	} catch (Exception e) {
+		e.printStackTrace();
+	}finally {
+		disConnection();
 	}
-	//2.총페이지()
-	public int boardtotalpage() {
-		int total=0;
-		
-		try {
-			getconnection();
-			String sql = "select ceil(count(*)/10.0 from jspboard";
-			ps= conn.prepareStatement(sql);
-			ResultSet rs= ps.executeQuery();
-			
-			if(rs.next()) {
-				total=rs.getInt(1);
-			}
-			rs.close();
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}finally {
-			disconnection();
-		}
-		return total;
-	}
-	
-	
+	   
+	   return vo;
+   }
+   
 }
--- boardproject/src/main/webapp/board/list.jsp
+++ boardproject/src/main/webapp/board/list.jsp
@@ -9,8 +9,8 @@
 	}
 	int curpage = Integer.parseInt(strpage);
 	//2.데이터베이스로 부터 데이터를 읽어 온다.
-	List<boardvo> list = dao.boardListdata(curpage);
-	int totalpage = dao.boardtotalpage();
+	List<boardvo> list = dao.boardListData(curpage);
+	int totalpage = dao.boardTotalPage();
 	
 	//페이지 나누기
 	final int block =3;
@@ -71,6 +71,7 @@
 				<%
 					for(boardvo vo : list){
 				%>
+				<tr>
 					<td class="text-center" width="10%"><%=vo.getNo() %></td>
 					<td width="45%"><a href="detail.jsp?no=<%=vo.getNo() %>"><%=vo.getSubject() %></a>
 					<sup style="color: red">new</sup>
@@ -78,11 +79,47 @@
 					<td class="text-center" width="15%"><%=vo.getName() %></td>
 					<td class="text-center" width="20%"><%=vo.getDbday() %></td>
 					<td class="text-center" width="10%"><%=vo.getHit() %></td>
+				</tr>
 			<% 		
 					}
 			%>
 			</table>
 		</div>
-	</div>
+			<div class="row">
+				<div class="text-center">
+							<ul class="pagination">
+								  <%
+								  	if(startpage>1){//1,,,4,5 다음페이지 출력흐름
+								  	
+								  %>
+								  <li><a href="list.jsp?page=<%=startpage-1%>">&lt</a></li>
+								  <%
+								  	}
+								  %>
+								  <!-- 페이지 바뀌는 소스 -->
+								  <%
+								  	for(int i=startpage; i<=endpage; i++){
+								  	String act = "";
+								  	if(curpage==i)
+								  		act="class=active";
+								  	else act="";
+								  	
+								  %>
+								  <li <%=act %>><a href="list.jsp?page=<%=i%>"><%=i %></a></li>
+								  <%
+								  	}
+								  %>
+								  <%
+								  	if(endpage<totalpage){
+								  %>
+								  <li><a href="list.jsp?page=<%=endpage+1%>">&gt</a></li>
+								  <%
+								  	}
+								  %>
+							</ul>
+				</div>
+			
+			</div>
+		</div>
 </body>
 </html>
